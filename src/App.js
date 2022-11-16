5c8fa1398e5475215ca947d4bc13f6f48621d440
Erlandino <erlend.kodehode@gmail.com>
2022-11-16 12:40:37 +0100

    creating new branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>front-page</h1>
+      </header>
     </div>
   );
 }

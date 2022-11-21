8cacb1c0f7af3656e803af226181587948dcbaab
Erlandino <erlend.kodehode@gmail.com>
2022-11-21 09:45:08 +0100

    projects skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Introduction from "./components/Introduction";
 import AboutMe from "./components/About_me";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Introduction />
       <AboutMe />
+      <Projects />
     </>
   );
 }

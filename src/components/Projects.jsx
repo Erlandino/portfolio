13691c984c27e7aeb14d66f07413711ff3cdd60c
Erlandino <erlend.kodehode@gmail.com>
2022-11-21 13:28:59 +0100

    imported carousel, styled carousel and buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,20 @@
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    // thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    // thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    // thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
 export default function Projects() {
   return (
     <section className="projects">
@@ -9,9 +26,7 @@ export default function Projects() {
           <button className="projects__container__navigation__button">Quiz site</button>
         </div>
         <div className="projects__container__carouselContainer">
-          <button className="projects__container__carouselContainer__button">{"<"}</button>
-          <div className="projects__container__carouselContainer__carousel"></div>
-          <button className="projects__container__carouselContainer__button">{">"}</button>
+          <ImageGallery items={images} />
         </div>
       </div>
     </section>

9e089a40c0108b60d27ed4f18304f51bdf57913a
Erlandino <erlend.kodehode@gmail.com>
2022-11-25 14:00:44 +0100

    fixed some classs names

--- src/components/About_me.jsx
+++ src/components/About_me.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function AboutMe() {
   return (
-    <section className="AboutMe">
-      <div className="AboutMe__container">
-        <h1 className="AboutMe__container__title">Om meg</h1>
-        <article className="AboutMe__container__article AboutMe__container__article--Koding">
-          <h2 className="AboutMe__container__article__title">Koding</h2>
-          <p className="AboutMe__container__article__paragraph">
+    <section className="aboutMe">
+      <div className="aboutMe__container">
+        <h1 className="aboutMe__container__title">Om meg</h1>
+        <article className="aboutMe__container__article aboutMe__container__article--Koding">
+          <h2 className="aboutMe__container__article__title">Koding</h2>
+          <p className="aboutMe__container__article__paragraph">
             Jeg liker å kode, og et av grunnen for dette er fordi at det gjør at jeg kan bruke kreativitet til å skape noe, for eksempel image karusell.
             <br />
             <br />
@@ -15,11 +15,11 @@ export default function AboutMe() {
           </p>
         </article>
 
-        <hr className="AboutMe__container__lineBreak" />
+        <hr className="aboutMe__container__lineBreak" />
 
-        <article className="AboutMe__container__article AboutMe__container__article--Hobbier">
-          <h2 className="AboutMe__container__article__title">Hobbier</h2>
-          <p className="AboutMe__container__article__paragraph">
+        <article className="aboutMe__container__article AboutMe__container__article--Hobbier">
+          <h2 className="aboutMe__container__article__title">Hobbier</h2>
+          <p className="aboutMe__container__article__paragraph">
             Når det kommer til hobbier så driver jeg med videospill i fritien. Akkurat nå er favorittspillet mitt warcraft 3. Jeg liker både det sosiale og utfordrene aspektene med videospill Og
             warcraft 3 lar meg oppleve begge deler.
           </p>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,23 @@
 export default function Contact() {
   return (
-    <section className="Contact">
-      <form action="" className="Contact__form">
+    <section className="contact">
+      <form action="" className="contact__form">
         <h2>Kontakt skjema</h2>
 
-        <label htmlFor="name" className="Contact__form__label">
+        <label htmlFor="name" className="contact__form__label">
           Navn
         </label>
-        <input type="text" id="name" name="name" className="Contact__form__input" />
+        <input type="text" id="name" name="name" className="contact__form__input" />
 
-        <label htmlFor="email" className="Contact__form__label">
+        <label htmlFor="email" className="contact__form__label">
           Email
         </label>
-        <input type="text" id="email" name="email" className="Contact__form__input" />
+        <input type="text" id="email" name="email" className="contact__form__input" />
 
-        <label htmlFor="message" className="Contact__form__label">
+        <label htmlFor="message" className="contact__form__label">
           Beskjed
         </label>
-        <textarea id="message" name="message" className="Contact__form__textarea"></textarea>
+        <textarea id="message" name="message" className="contact__form__textarea"></textarea>
 
         <button>Send mail</button>
       </form>

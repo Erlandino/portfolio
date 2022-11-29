a3f04c2460236ecad698dfde7790543c48e471a9
Erlandino <erlend.kodehode@gmail.com>
2022-11-29 14:48:00 +0100

    added hamburger navbar, styled navbar, made it a bit more responsive

---
+++ src/components/Front-page/Navbar.jsx
@@ -0,0 +1,44 @@
+import { HashLink } from "react-router-hash-link";
+import hamburgerMenu from "../../media/menu-hamburger.svg";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(true);
+  return (
+    <nav className="nav">
+      <ul className="nav__ul">
+        {showNavbar && (
+          <li className="nav__ul__li">
+            <HashLink className="nav__ul__li__a" smooth to="/path#introduction">
+              Home
+            </HashLink>
+          </li>
+        )}
+        {showNavbar && (
+          <li className="nav__ul__li">
+            <HashLink className="nav__ul__li__a" smooth to="/path#aboutMe">
+              About me
+            </HashLink>
+          </li>
+        )}
+        {showNavbar && (
+          <li className="nav__ul__li">
+            <HashLink className="nav__ul__li__a" smooth to="/path#projects">
+              Projects
+            </HashLink>
+          </li>
+        )}
+        {showNavbar && (
+          <li className="nav__ul__li">
+            <HashLink className="nav__ul__li__a" smooth to="/path#contact">
+              Contact
+            </HashLink>
+          </li>
+        )}
+        <li className="nav__ul__li">
+          <img src={hamburgerMenu} alt="Hamburger navbar menu" className="nav__ul__li__img" onClick={() => setShowNavbar((prevState) => !prevState)} />
+        </li>
+      </ul>
+    </nav>
+  );
+}

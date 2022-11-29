aaa1883f9a80b39d1ed51846fa1063fe3d209c4d
Erlandino <erlend.kodehode@gmail.com>
2022-11-29 12:42:07 +0100

    finished setstate for gallery, added github/figma links below project gallery, moved some elements from gallery component to project component

--- src/components/Projects/Gallery.jsx
+++ src/components/Projects/Gallery.jsx
@@ -1,7 +1,7 @@
 import ImageGallery from "react-image-gallery";
 
 export default function Gallery(props) {
-  const { projectName, firstImage, secondImage, thirdImage, githubLink, figmaLink } = props;
+  const { firstImage, secondImage, thirdImage } = props;
 
   const images = [
     {
@@ -18,16 +18,8 @@ export default function Gallery(props) {
     },
   ];
   return (
-    <div className="projects__container">
-      <h1 className="projects__container__title">Projekter</h1>
-      <div className="projects__container__navigation">
-        <button className={`projects__container__navigation__button ${projectName === "Flywing" && "projects__container__navigation__button-active"}`}>Travel website</button>
-        <button className={`projects__container__navigation__button ${projectName === "StreamingSite" && "projects__container__navigation__button-active"}`}>Streaming site design</button>
-        <button className={`projects__container__navigation__button ${projectName === "solar Quiz" && "projects__container__navigation__button-active"}`}>Quiz site</button>
-      </div>
-      <div className="projects__container__carouselContainer">
-        <ImageGallery items={images} />
-      </div>
+    <div className="projects__container__carouselContainer">
+      <ImageGallery items={images} />
     </div>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,25 @@
 import Gallery from "./Gallery";
 
-// flywing images imports
+// Flywing images imports
 import flywingImageOne from "../../media/flywing/flywing-frontPage.png";
 import flywingImageTwo from "../../media/flywing/flywing-imageGallery.png";
 import flywingImageThree from "../../media/flywing/flywing-planeDelayChart.png";
 
-// solar quiz images imports
+// Solar quiz images imports
 import solarQuizImageOne from "../../media/solar-quiz/solar-quiz-registration.png";
-import solarQuizImageTwo from "../../media/solar-quiz/solar-quiz-registration.png";
-import solarQuizImageThree from "../../media/solar-quiz/solar-quiz-registration.png";
+import solarQuizImageTwo from "../../media/solar-quiz/solar-quiz-result.png";
+import solarQuizImageThree from "../../media/solar-quiz/solar-quiz.png";
+
+// Streaming site images imports
+import StreamingSiteImageOne from "../../media/newNexus/newNexus-desktop-contact.png";
+import StreamingSiteImageTwo from "../../media/newNexus/newNexus-desktop-main-menu.png";
+import StreamingSiteImageThree from "../../media/newNexus/newNexus-desktop-main.png";
+
+// Githhub icon import
+import githubIcon from "../../media/GitHub-Mark-64px.png";
+
+// Figma icon
+import figmaIon from "../../media/figma-logo.svg";
 
 import { useState } from "react";
 
@@ -22,32 +33,73 @@ export default function Projects() {
       github: "https://github.com/Erlandino/portofolio",
       figma: "",
     },
-
+    {
+      projectName: "Streaming site",
+      imageOne: StreamingSiteImageOne,
+      imageTwo: StreamingSiteImageTwo,
+      imageThree: StreamingSiteImageThree,
+      github: "",
+      figma: "https://www.figma.com/file/EG0HAnVadfGMGMQDkvoX8y/Design-quiz-site?node-id=40%3A4&t=iBoCdhVxGMewcCKF-1",
+    },
     {
       projectName: "solar Quiz",
       imageOne: solarQuizImageOne,
       imageTwo: solarQuizImageTwo,
       imageThree: solarQuizImageThree,
       github: "https://github.com/Erlandino/autmn-project",
-      figma: "",
+      figma: "https://www.figma.com/file/EG0HAnVadfGMGMQDkvoX8y/Design-quiz-site?node-id=40%3A4&t=iBoCdhVxGMewcCKF-1",
     },
   ];
 
-  const [imageSet, setImageSet] = useState(projects[0]);
+  const [imageSet, setImageSet] = useState(0);
 
-  console.log(imageSet);
+  function imageGalleryProjectSelect(index) {
+    setImageSet((prevImageSet) => index);
+  }
+  console.log(githubIcon);
 
-  // console.log(imageSet.flywing && "yup");
-  // console.log(imageSet.solarQuiz && "yup");
-  // setImageSet((prevImageSet) => {
-  //   if (prevImageSet.flywing) {
-  //   }
-  // });
-
-  const { imageOne, imageTwo, imageThree, github, figma, projectName } = imageSet;
+  const { imageOne, imageTwo, imageThree, github, figma, projectName } = projects[imageSet];
   return (
     <section className="projects" id="projects">
-      <Gallery projectName={projectName} firstImage={imageOne} secondImage={imageTwo} thirdImage={imageThree} githubLink={github} figmaLink={figma} />
+      <div className="projects__container">
+        <h1 className="projects__container__title">Projekter</h1>
+        <div className="projects__container__navigation">
+          <button className={`projects__container__navigation__button ${projectName === "Flywing" && "projects__container__navigation__button-active"}`} onClick={() => imageGalleryProjectSelect(0)}>
+            Travel website
+          </button>
+          <button
+            className={`projects__container__navigation__button ${projectName === "Streaming site" && "projects__container__navigation__button-active"}`}
+            onClick={() => imageGalleryProjectSelect(1)}
+          >
+            Streaming site design
+          </button>
+          <button
+            className={`projects__container__navigation__button ${projectName === "solar Quiz" && "projects__container__navigation__button-active"}`}
+            onClick={() => imageGalleryProjectSelect(2)}
+          >
+            Quiz site
+          </button>
+        </div>
+        <Gallery firstImage={imageOne} secondImage={imageTwo} thirdImage={imageThree} />
+        <ul>
+          {github && (
+            <li>
+              <a href={github}>
+                <img src={githubIcon} alt="" />
+                <h2>{projectName} github repository</h2>
+              </a>
+            </li>
+          )}
+          {figma && (
+            <li>
+              <a href={figma}>
+                <img src={figmaIon} alt="" />
+                <h2>{projectName} figma folder</h2>
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
     </section>
   );
 }

dc759b93eaa090924123a09d6c64c5a11cfd875b
Erlandino <erlend.kodehode@gmail.com>
2022-12-05 11:33:33 +0100

    made background colour on navbar responsive to, cleaned up code

--- src/components/Front-page/Navbar.jsx
+++ src/components/Front-page/Navbar.jsx
@@ -1,5 +1,4 @@
 import { HashLink } from "react-router-hash-link";
-import hamburgerMenu from "../../media/menu-hamburger.svg";
 import { useState, useEffect } from "react";
 
 export default function Navbar() {
@@ -47,8 +46,11 @@ export default function Navbar() {
             </HashLink>
           </li>
         )}
-        <li className="nav__ul__li" onClick={() => setShowNavbar((prevState) => !prevState)}>
-          <img src={hamburgerMenu} alt="Hamburger navbar menu" className="nav__ul__li__menu" />
+        <li className="nav__ul__li" style={{ borderRadius: `${!showNavbar ? "0 0 0 30px" : ""}` }} onClick={() => setShowNavbar((prevState) => !prevState)}>
+          <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg" alt="Hamburger navbar menu" className="nav__ul__li__menu">
+            <path d="M7.21875 32.6562H34.7812M7.21875 22.1562H34.7812M7.21875 11.6562H34.7812" stroke="black" onMouseOver={() => console.log("test")} />
+          </svg>
+          {/* <img src={hamburgerMenu} alt="Hamburger navbar menu" className="nav__ul__li__menu" /> */}
         </li>
       </ul>
     </nav>

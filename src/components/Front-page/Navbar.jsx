db3905f09e110b26fd3c1f05c1469be6dcd57f92
Erlandino <erlend.kodehode@gmail.com>
2022-12-05 14:29:07 +0100

    added hover and active state to hamburger menu in navbar

--- src/components/Front-page/Navbar.jsx
+++ src/components/Front-page/Navbar.jsx
@@ -3,6 +3,13 @@ import { useState, useEffect } from "react";
 
 export default function Navbar() {
   const [showNavbar, setShowNavbar] = useState(true);
+  const [ifHover, setIfHover] = useState(false);
+
+  function onHover() {
+    if (window.innerWidth > 600) {
+      setIfHover((prevState) => !prevState);
+    }
+  }
 
   useEffect(() => {
     window.innerWidth <= 600 && setShowNavbar(false);
@@ -46,9 +53,28 @@ export default function Navbar() {
             </HashLink>
           </li>
         )}
-        <li className="nav__ul__li" style={{ borderRadius: `${!showNavbar ? "0 0 0 30px" : ""}` }} onClick={() => setShowNavbar((prevState) => !prevState)}>
-          <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg" alt="Hamburger navbar menu" className="nav__ul__li__menu">
-            <path d="M7.21875 32.6562H34.7812M7.21875 22.1562H34.7812M7.21875 11.6562H34.7812" stroke="black" onMouseOver={() => console.log("test")} />
+        <li
+          onMouseOver={onHover}
+          onMouseLeave={onHover}
+          className="nav__ul__li nav__ul__li--menuBackground"
+          style={{
+            borderRadius: `${!showNavbar ? "0 0 0 30px" : ""}`,
+            backgroundColor: `${ifHover || showNavbar ? "#3a4856" : "white"}`,
+          }}
+          onClick={() => setShowNavbar((prevState) => !prevState)}
+        >
+          <svg
+            onMouseOver={onHover}
+            onMouseLeave={onHover}
+            width="42"
+            height="43"
+            viewBox="0 0 42 43"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            alt="Hamburger navbar menu"
+            className="nav__ul__li__menu"
+          >
+            <path d="M7.21875 32.6562H34.7812M7.21875 22.1562H34.7812M7.21875 11.6562H34.7812" stroke={ifHover || showNavbar ? "white" : "black"} />
           </svg>
           {/* <img src={hamburgerMenu} alt="Hamburger navbar menu" className="nav__ul__li__menu" /> */}
         </li>

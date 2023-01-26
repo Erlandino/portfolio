9cc773f6ec9c82969efa7ae0d38c4400ae4c1e78
Erlandino <erlend.kodehode@gmail.com>
2023-01-26 12:33:46 +0100

    fixed spelling error

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -15,7 +15,7 @@ export default function Introduction() {
           <h1 className="introduction__container__text__title">Hei</h1>
           {/* Paragraph */}
           <p className="introduction__container__text__paragraph">
-            mitt navn er <strong>Erlend</strong> og jeg er en front-end utvikler
+            mitt navn er <strong>Erlend</strong> og jeg er en frontend utvikler
           </p>
         </div>
         {/* Img element containing a photo of myself */}
